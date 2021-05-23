dce846c3930361f48dcea6956edbe5131a823d60
allip <1145746688@qq.com>
2021-05-23 15:56:25 +0800

    vite配置 预留esbuild 和css 配置项

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+    // css:{},
+    // esbuild:{},
     alias: {
         '@': path.resolve(__dirname, 'src'),
         comps: path.resolve(__dirname, 'src/components'),
