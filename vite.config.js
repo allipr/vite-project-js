3dfb1f778f43351fc4adfde966198d3b843fe6bd
allip <1145746688@qq.com>
2021-05-23 16:57:21 +0800

    resolve.alias定义别名

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+const { resolve } = path
 // https://vitejs.dev/config/
 export default defineConfig({
     // css:{},
     // esbuild:{},
-    alias: {
-        '@': path.resolve(__dirname, 'src'),
-        comps: path.resolve(__dirname, 'src/components'),
-        apis: path.resolve(__dirname, 'src/apis'),
-        views: path.resolve(__dirname, 'src/views'),
-        utils: path.resolve(__dirname, 'src/utils'),
-        routes: path.resolve(__dirname, 'src/routes'),
-        styles: path.resolve(__dirname, 'src/styles'),
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            comps: resolve(__dirname, 'src/components'),
+            apis: resolve(__dirname, 'src/apis'),
+            views: resolve(__dirname, 'src/views'),
+            utils: resolve(__dirname, 'src/utils'),
+            routes: resolve(__dirname, 'src/routes'),
+            styles: resolve(__dirname, 'src/styles'),
+        },
     },
+
     plugins: [vue()],
 })
