0728dcb8ed65e53585fae15538f4ae4c42e39c4e
allip <1145746688@qq.com>
2021-05-23 15:52:58 +0800

    vite配置 添加alias别名配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    alias: {
+        '@': path.resolve(__dirname, 'src'),
+        comps: path.resolve(__dirname, 'src/components'),
+        apis: path.resolve(__dirname, 'src/apis'),
+        views: path.resolve(__dirname, 'src/views'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        routes: path.resolve(__dirname, 'src/routes'),
+        styles: path.resolve(__dirname, 'src/styles'),
+    },
+    plugins: [vue()],
 })
